c512e4ba8ab171dc1b50550d671c8a4dd14c3b7d
chvijayalakshmi <challavijayalakshmi8@gmail.com>
2023-04-15 23:02:46 +0200

    Initial commit

---
+++ src/test/java/com/example/Commitpushdemo/CommitPushDemoApplicationTests.java
@@ -0,0 +1,58 @@
+package com.example.Commitpushdemo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitPushDemoApplicationTests {
+	CommitPushDemoApplication c = new CommitPushDemoApplication();
+	@Test
+	void additionShouldBeTrue(){
+		int a=2,b=3;
+		int Expected =5;
+		int Result = c.add(a,b);
+		Assertions.assertEquals(Result,Expected);
+	}
+	@Test
+	void sub(){
+		int a=15;
+		int b=5;
+		int Expected = 10;
+		int Result = c.subtract(a,b);
+		Assertions.assertEquals(Result,Expected);
+	}
+	@Test
+	void multiplication(){
+		int a=2;
+		int b=4;
+		int Expected = 8;
+		int Result = c.multiply(a,b);
+		Assertions.assertEquals(Result,Expected);
+	}
+	@Test
+	void division(){
+		int a=10;
+		int b=2;
+		int Expected = 5;
+		double Result = c.divide(a,b);
+		Assertions.assertEquals(Result,Expected);
+	}
+	@Test
+	void squareRute(){
+		int a=2;
+		double Result = c.squareRootOf(a);
+		double Expected = 1.4142135623730951;
+
+		Assertions.assertEquals(Result,Expected);
+	}
+	@Test
+	void area(){
+		int a=2;
+		double Result = c.getArea(a);
+		double Expected = 12.57;
+
+		Assertions.assertEquals(Result,Expected);
+	}
+
+}
